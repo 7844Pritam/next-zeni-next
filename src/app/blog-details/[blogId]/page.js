b100711845ff15a71e27a00731608a32feb24a30
7844Pritam <pritamsharma7844@gmail.com>
2025-06-08 19:41:13 +0530

    save

--- src/app/blog-details/[blogId]/page.js
+++ src/app/blog-details/[blogId]/page.js
@@ -19,55 +19,6 @@ import { useParams } from "next/navigation";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
-export async function generateMetadata({ params }) {
-  const blogId = params.blogId;
-
-  try {
-    const blogRef = doc(db, "blogs", blogId);
-    const blogSnap = await getDoc(blogRef);
-
-    if (!blogSnap.exists()) {
-      return {
-        title: "Blog Not Found",
-        description: "The blog you're looking for does not exist.",
-      };
-    }
-
-    const blog = blogSnap.data();
-
-    return {
-      title: blog.title || "Untitled Blog",
-      description: blog.subtitle || "Check out this blog post.",
-      openGraph: {
-        title: blog.title || "Untitled Blog",
-        description: blog.subtitle || "Check out this blog post.",
-        url: `https://next-zeni-next.vercel.app/blog/${blogId}`,
-        images: blog.image
-          ? [
-              {
-                url: blog.image,
-                width: 1200,
-                height: 630,
-                alt: blog.title || "Blog preview",
-              },
-            ]
-          : [],
-      },
-      twitter: {
-        card: "summary_large_image",
-        title: blog.title || "Untitled Blog",
-        description: blog.subtitle || "Check out this blog post.",
-        images: blog.image ? [blog.image] : [],
-      },
-    };
-  } catch (error) {
-    console.error("Error generating metadata:", error.message);
-    return {
-      title: "Blog Error",
-      description: "There was a problem generating blog metadata.",
-    };
-  }
-}
 
 const Page = () => {
   const { blogId } = useParams();
