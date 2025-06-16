99a744a9635513fef483b5c0e32636a647b3c4be
7844Pritam <pritamsharma7844@gmail.com>
2025-06-16 19:57:57 +0530

    save

--- src/app/components/blog/ShowBlogs.jsx
+++ src/app/components/blog/ShowBlogs.jsx
@@ -3,7 +3,6 @@ import { db } from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import "react-quill/dist/quill.snow.css";
 
 const ShowBlogs = () => {
   const [blogs, setBlogs] = useState([]);
